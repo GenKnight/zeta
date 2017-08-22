9c10cee81e705f181fc8e333ba453273924480fb
penguin <kaishansheng@163.com>
2017-08-22 14:09:35 +0800

    update

--- google2/protobuf/bin/proto.bat
+++
@@ -1,7 +0,0 @@
-set in_dir=D:\work\protobuf_app\proto
-set out_dir=D:\work\protobuf_app\proto
-
-"D:\work\protobuf_app\google\protobuf\bin\protoc.exe" --proto_path=%in_dir% --cpp_out=%out_dir% ^
-%in_dir%\basehead.proto ^
-%in_dir%\protocal.proto
-pause
--- proto/proto.bat
+++ proto/proto.bat
@@ -3,5 +3,6 @@ set out_dir=D:\work\zeta\proto
 
 "D:\work\zeta\google\protobuf\bin\protoc.exe" --proto_path=%in_dir% --cpp_out=%out_dir% ^
 %in_dir%\basehead.proto ^
-%in_dir%\protocal.proto
+%in_dir%\protocal.proto ^
+%in_dir%\risk_protocal.proto
 pause
